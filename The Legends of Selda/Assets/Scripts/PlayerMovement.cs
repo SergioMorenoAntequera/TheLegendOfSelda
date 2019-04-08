479dc126f4011946ef009f2b6fb3ad7e2de664d9
Sergio <seranmoreno500@gmail.com>
2019-04-08 21:42:50 +0200

    Project created and assets choosed
    first commit

---
+++ The Legends of Selda/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    private Rigidbody2D rb;
+
+    public void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += new Vector2(movementSpeed, 0);
+
+            Debug.Log("Velocity: " + rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += new Vector2(-movementSpeed, 0);
+            Debug.Log("Velocity: " + rb.velocity.y);
+        }
+    }
+
+}
